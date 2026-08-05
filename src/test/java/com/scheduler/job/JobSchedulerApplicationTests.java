68ee2333b97ae9610bbf97000e93b8e50f63f832
gauransh2112 <nuwalgauransh7@gmail.com>
2026-08-05 13:44:39 +0530

    feat: add local infrastructure foundation

--- src/test/java/com/scheduler/job/JobSchedulerApplicationTests.java
+++ src/test/java/com/scheduler/job/JobSchedulerApplicationTests.java
@@ -3,13 +3,7 @@ package com.scheduler.job;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-    "spring.autoconfigure.exclude=" +
-    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration"
-})
+@SpringBootTest
 class JobSchedulerApplicationTests {
 
     @Test
